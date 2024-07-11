970bffb37d08230425ba0093694496f83a0a6bcc
mauri <mauricio.barrera.094@gmail.com>
2024-07-11 19:15:00 -0300

    react

---
+++ front/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="holder">
+        <img src="img/logo.png" width="100" alt="Cuartel de Bomberos Z" />
+        <h1>Cuartel de Bomberos Z</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
